d5605255a7d276dd2c5c552336efef8ab8de11c9
adekcz <adekcz@gmail.com>
2022-04-24 21:30:53 +0200

    rendering grid

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const size = 5;
+
+function Tile(props) {
+    return (
+        <button className="tile" onClick={props.onClick}>
+        {props.value}
+        </button>
+    );
+
+}
+
+class Board extends React.Component {
+
+    renderTile(row, col) {
+        return <Tile
+        value={this.props.tiles[row][col]} 
+        onClick={() => this.props.onClick(row, col)}
+            />;
+    }
+
+    render() {
+        return (
+            <div>
+            { this.props.tiles.map(
+                (row, rowId) => 
+                    (<div className="board-row">
+                        {
+                            row.map((col, colId) => 
+                                this.renderTile(rowId, colId)
+                            )
+                        }
+                    </div>
+                    )
+                )
+            }
+            </div>
+        )
+    }
+}
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.tiles = Array(size).fill(null);
+        for (let row = 0; row<size; row++) {
+            this.tiles[row] = Array(size).fill(null);
+        }
+        for(let i = 0; i<size;i++)
+            for(let j = 0; j<size;j++)
+                this.tiles[i][j] = i+","+j;
+    }
+
+    render() {
+        return (
+            <Board tiles={this.tiles}
+            />
+        );
+    }
 }
 
 export default App;
